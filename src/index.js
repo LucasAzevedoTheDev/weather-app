a5a2ead40e3c8471e676a4ae59b90e94674dd59c
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-09-23 07:53:44 -0300

    add inputButton event and error span

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import "./styles.css";
 import {getWeather} from "./request.js";
 
-getWeather("los angeles");
+const input = document.querySelector(".input");
+const inputButton = document.querySelector(".input-button");
+const errorSpan = document.querySelector("span");
+
+inputButton.addEventListener("click", () => {
+  let value = input.value;
+  
+  if(!value) {
+    errorSpan.classList.add("error");
+  } else {
+    getWeather(value);
+    errorSpan.classList.remove("error");
+  }
+});
+
+// getWeather("los angeles");
