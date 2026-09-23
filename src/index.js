5b1f2fb9ee859bef4d47c7c0d0266fe1cc8f72df
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-09-23 11:06:30 -0300

    refactor getWeather function so it returns the data

--- src/index.js
+++ src/index.js
@@ -12,9 +12,11 @@ inputButton.addEventListener("click", async () => {
     errorSpan.classList.add("error");
   } else {
     try {
-      const fetched = await getWeather(value);
+      const fetched = await getWeather(value);  
       errorSpan.classList.remove("error"); 
-    } catch {
+      console.log(fetched);
+      // INSERT RENDER FUNCTION FROM UI HERE
+    } catch(error) {
       errorSpan.classList.add("error");
       errorSpan.textContent = "Please enter a valid location.";
     }
--- src/request.js
+++ src/request.js
@@ -2,10 +2,10 @@ async function getWeather(location) {
   try {
     const requested = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=9Z35HWXREP2DL48JVDJRXP8DK`);
     const data = await requested.json();
-    console.log(data);
+    return data;
   }
   catch(error) {
-    console.log(error);
+    console.error(error);
   }
 }
 
