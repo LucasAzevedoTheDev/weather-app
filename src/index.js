ba8d1ff429822e1b25bb9a58d88f370e60b1dc2f
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-09-23 08:08:57 -0300

    improve api error handling

--- src/index.js
+++ src/index.js
@@ -1,19 +1,2 @@
 import "./styles.css";
 import {getWeather} from "./request.js";
-
-const input = document.querySelector(".input");
-const inputButton = document.querySelector(".input-button");
-const errorSpan = document.querySelector("span");
-
-inputButton.addEventListener("click", () => {
-  let value = input.value;
-  
-  if(!value) {
-    errorSpan.classList.add("error");
-  } else {
-    getWeather(value);
-    errorSpan.classList.remove("error");
-  }
-});
-
-// getWeather("los angeles");
--- src/request.js
+++ src/request.js
@@ -1,12 +1,29 @@
+const input = document.querySelector(".input");
+const inputButton = document.querySelector(".input-button");
+const errorSpan = document.querySelector("span");
+let data;
+
 async function getWeather(location) {
   try {
     const requested = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=9Z35HWXREP2DL48JVDJRXP8DK`);
-    const data = await requested.json();
+    data = await requested.json();
     console.log(data);
   }
   catch(error) {
-    console.error(error);
+    errorSpan.classList.add("error");
+    errorSpan.textContent = "Please enter a valid location.";
+    console.log(error);
   }
 }
 
-export {getWeather};
+inputButton.addEventListener("click", () => {
+  let value = input.value;
+  
+  if(!value) {
+    errorSpan.classList.add("error");
+  } else {
+    getWeather(value);
+    errorSpan.classList.remove("error");
+  }
+});
+export {getWeather, data};
