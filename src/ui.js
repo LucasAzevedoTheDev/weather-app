1f6e74ab6a9fbb12bf982677e84a4bd94cd6533a
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-09-23 21:46:49 -0300

    render API temperature, feels like and condition to ui

--- src/ui.js
+++ src/ui.js
@@ -6,11 +6,25 @@ const errorSpan = document.querySelector("span");
 const container = document.querySelector(".container");
 
 function renderWeather(data) {
-  const locationName = document.createElement("h2");
+  const locationName = document.createElement("p");
   locationName.classList.add("location-name");
   locationName.textContent = data.resolvedAddress;
-  console.log(data.resolvedAddress);
   container.appendChild(locationName);
+
+  const locationFeelsLike = document.createElement("p");
+  locationFeelsLike.classList.add("location-feels-like");
+  locationFeelsLike.textContent = `feels like ${data.currentConditions.feelslike}°`;
+  container.appendChild(locationFeelsLike);
+
+  const locationTemp = document.createElement("p");
+  locationTemp.classList.add("location-temp");
+  locationTemp.textContent = `${data.currentConditions.temp}°`;
+  container.appendChild(locationTemp);
+
+  const locationConditions = document.createElement("p");
+  locationConditions.classList.add("location-conditions");
+  locationConditions.textContent = data.currentConditions.conditions;
+  container.appendChild(locationConditions);
 }
 
 export {input, inputButton, errorSpan, renderWeather};
